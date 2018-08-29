9d0a0b33051d7abf20b8d039200957781b2bd69f
abielen <bielenalexander@gmail.com>
2018-08-29 22:07:27 +0200

    Initial commit

---
+++ src/main/java/org/parabot/scriptwriter/revival/antiban/WhitelistAntiBan.java
@@ -0,0 +1,45 @@
+package org.parabot.scriptwriter.revival.antiban;
+
+import org.parabot.environment.api.utils.Time;
+import org.parabot.environment.scripts.framework.Strategy;
+import org.rev317.min.api.methods.Players;
+import org.rev317.min.api.wrappers.Player;
+
+import java.util.ArrayList;
+
+public class WhitelistAntiBan implements Strategy {
+    private ArrayList<String> whitelist;
+    private boolean enabled = true;
+    public WhitelistAntiBan() {
+        WhitelistForm whitelistForm = new WhitelistForm();
+        while(whitelistForm.isVisible()) {
+            Time.sleep(1000);
+        }
+
+        whitelist = whitelistForm.getNames();
+        if(whitelist == null) {
+            enabled = false;
+        } else {
+            whitelist.add(Players.getMyPlayer().getName().toLowerCase());
+        }
+    }
+
+    @Override
+    public boolean activate() {
+        return enabled && playersClose();
+    }
+
+    @Override
+    public void execute() {
+        Time.sleep(1000);
+    }
+
+    private boolean playersClose() {
+        for(Player p : Players.getPlayers()) {
+            if(!whitelist.contains(p.getName().toLowerCase())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
